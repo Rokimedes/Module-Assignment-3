ffac4f9dd6dd2c36390fa77981b95ce5ea8e43c5
Rokimedes <36521512+Rokimedes@users.noreply.github.com>
2018-12-02 07:01:55 +0100

    Finished Level 1 Q1 and Q2

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,51 @@
 //Use RegEx to validate form
+
+document.getElementById("submit").addEventListener("click",  function() {
+    var firstName = document.getElementById("fname").value;
+    var firstNameRGEX = /^[a-zA-Z ]+$/;
+    var firstNameResult = firstNameRGEX.test(firstName);
+    console.log("First Name is: " + firstNameResult);
+    
+    var lastName = document.getElementById("lname").value;
+    var lastNameRGEX = /^[a-zA-Z ]+$/;
+    var lastNameResult = lastNameRGEX.test(lastName);
+    console.log("Last Name is: " + lastNameResult);
+
+    var phoneNumber = document.getElementById("phone").value;
+    var phoneNumberRGEX = /\d{8,12}/;
+    var phoneNumberResult = phoneNumberRGEX.test(phoneNumber);
+    console.log("Phone number is: " + phoneNumberResult);
+
+    var email = document.getElementById("email").value;
+    var emailRGEX = /[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-_]+\.[a-zA-Z]{2,4}/;
+    var emailResult = emailRGEX.test(email);
+    console.log("E-mail is: " + emailResult);
+
+});
+
+
+
+
+// The code that is commented below is one option on how to do it with the pattern tag 
+// in HTML, but was unsure on how we should use so i went with the "hard way"
+// of doing it, with coding out everything.
+
+
+
+
+// document.getElementById("submit").addEventListener("click", function(){
+
+//     var inputField = document.getElementsByTagName("input");
+
+//     for (var index = 0; index < inputField.length; index++) {
+//         var singleInput = inputField[index].value;
+//         var inputPattern = new RegExp(inputField[index].pattern);
+//         var testing = inputPattern.test(singleInput);
+
+//         if(testing === false) {
+//             console.log(inputField[index].name + " is incorrect");
+//         }
+        
+//     }
+// });
+
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,44 @@
 //Console.log out elements in JSON file
+
+var videoObject = { // Had to add it to an variable to not get an error for first "video":
+	"video": [{
+		"id": 12312412312,
+		"name": "Ecuaciones Diferenciales",
+		"url": "/video/math/edo/12312412312",
+		"author": {
+			"data": [{
+				"name_author": "Alejandro Morales",
+				"url": "/author/alejandro-morales",
+				"type": "master"
+			}]
+		}
+	}]
+};
+
+(function () {
+    for (var i = 0; i < videoObject.video.length; i++) {
+
+        for (key in videoObject.video[i]) {
+            if (videoObject.video[i].hasOwnProperty(key)) {
+                if (key !== "author") { // Hide the Author object to not show as [Object] [Object]
+                console.log(key + ": " + videoObject.video[i][key]);
+                }
+            };
+
+        };
+
+    };
+
+    var authorArray = videoObject.video[0].author.data;
+
+    for (var j = 0; j < authorArray.length; j++) {
+
+        for (key in authorArray[j]) {
+            if (authorArray[j].hasOwnProperty(key)) {
+                console.log(key + ": " + authorArray[j][key]);
+            };
+
+        };
+
+    };
+})();
